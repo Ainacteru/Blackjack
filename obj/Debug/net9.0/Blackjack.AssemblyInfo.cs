92a7eb9ad0ea27bc8ab96b8b8cf6051b7ccdc72e
ainacteru <gbenawijr@gmail.com>
2025-07-30 22:00:55 -0500

    evil add

--- obj/Debug/net9.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d234bfa63b0b98f35e73ff0473e2358a30d97418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1d9a6e1b57a2724a06bd6562ad1ffb8dd5340a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
